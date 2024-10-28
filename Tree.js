68b2ae72012c2715f503daaf834fc02623fe49fd
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-10-27 23:45:47 -0600

    finish buildTreeRecursion function
    works fine and i didnt need to look at the
    code i copied earlier when learning BST

--- Tree.js
+++ Tree.js
@@ -3,11 +3,20 @@ import Node from "./Node.js";
 class Tree {
   #root;
   constructor(arr) {
-    this.#root = this.buildTreeRecursion(arr);
+    this.#root = this.buildTreeRecursion(arr, 0, arr.length);
     // this.#root = this.buildTreeQueue(arr);
   }
 
-  buildTreeRecursion(arr) {}
+  buildTreeRecursion(arr, start, end) {
+    if (start > end) return null;
+    let mid = Math.floor((start + end) / 2);
+    let root = new Node(arr[mid]);
+
+    root.left = this.buildTreeRecursion(arr, start, mid - 1);
+    root.right = this.buildTreeRecursion(arr, mid + 1, end);
+
+    return root;
+  }
 
   buildTreeQueue(arr) {}
 }
