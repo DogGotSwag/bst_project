4bca2430359cc95c3813c02e10f61e4b154afd4b
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-10-29 01:18:46 -0600

    complete queue version of buildTree function
    complete the function it did take some mental
    struggle but me figuring it out by myself instead
    of looking at code helped me understand whats
    happening much better also already tested it
    multiple sized arrays

--- Tree.js
+++ Tree.js
@@ -3,7 +3,7 @@ import Node from "./Node.js";
 class Tree {
   #root;
   constructor(arr) {
-    this.#root = this.buildTreeRecursion(arr, 0, arr.length-1);
+    // this.#root = this.buildTreeRecursion(arr, 0, arr.length-1);
     this.#root = this.buildTreeQueue(arr);
   }
 
@@ -20,15 +20,24 @@ class Tree {
 
   buildTreeQueue(arr) {
     if (arr.length === 0) return null;
-    let mid = Math.floor((0 + arr.length-1) / 2);
-    let root = new Node(arr[mid]);
+    let root = new Node(arr[Math.floor((0 + arr.length-1) / 2)]);
     let queue = [{ node: root, range: [0, arr.length-1] }];
     let front = 0;
 
     while (front < queue.length) {
       let current = queue[front].node;
       let [s,e] = queue[front].range;
-      console.log(s,e);
+      let m = Math.floor((s+e)/2); 
+
+      if(s<m){
+        current.left = new Node(arr[Math.floor((s+m-1)/2)]);
+        queue.push({node: current.left, range:[s,m-1]}); 
+      }
+
+      if(m<e){
+        current.right = new Node(arr[Math.floor((m+1+e)/2)]);
+        queue.push({node: current.right, range:[m+1,e]}); 
+      }
       
       front++;
     }
